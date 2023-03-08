33bf437829b1bb03dff8691238db5539ec95b12f
Pantakit <mr.pantakit@gmail.com>
2023-03-08 17:10:09 +0700

    .

--- app/Views/default_layout.php
+++ app/Views/default_layout.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ระบบยืนยันตัวตนในการเข้าใช้งานอินเตอร์เน็ต วิทยาลัยเทคนิคชัยภูมิ</title>
     <?= csrf_meta() ?>
 
     <link rel="stylesheet" href="<?= base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
